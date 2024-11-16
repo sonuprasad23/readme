5ee92fd54a8bcf0b61debc2f397b1f85385fd042
Sonu Prasad <sonuprasadmahato31@gmail.com>
2024-11-16 06:10:45 +0000

    Yeah Work done

---
+++ Java/Hello.java
@@ -0,0 +1,5 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("I was not expecting this much in my life and in such a way i never thought i was gonna become like this");
+    }
+}
